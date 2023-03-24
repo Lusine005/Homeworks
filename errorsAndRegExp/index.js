757542fd6240244fdf0d56c827644dbf6d08d894
Lusine005 <127478819+Lusine005@users.noreply.github.com>
2023-03-24 14:05:04 +0400

    add Date homework

---
+++ errorsAndRegExp/index.js
@@ -0,0 +1,8 @@
+function dayOfYear(initialDate) {
+  const now = new Date(initialDate);
+  const start = new Date(now.getFullYear(), 0, 0);
+  return (now - start) / 1000 / 60 / 60 / 24;
+}
+
+console.log(dayOfYear("12/13/2020")) // 348
+console.log(dayOfYear("12/17/2020")) // 352
